63391918db8a208ff0fd79b2c44ce49f13a92dfe
Arjun-Jyothidas <169043806+Arjun-Jyothidas@users.noreply.github.com>
2024-05-06 16:50:10 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,562 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include<stdio.h>
+#include "string.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+TIM_HandleTypeDef htim16;
+
+UART_HandleTypeDef huart1;
+
+HCD_HandleTypeDef hhcd_USB_DRD_FS;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void SystemPower_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ICACHE_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_USB_DRD_FS_HCD_Init(void);
+static void MX_TIM16_Init(void);
+/* USER CODE BEGIN PFP */
+
+void us_delay(int us);
+void SET_PIN_In();
+void SET_PIN_Out();
+uint8_t DS18B20_Start();
+uint8_t DS18B20_Write(uint8_t data);
+uint16_t DS18B20_Read();
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+int Check_Time = 0;
+uint8_t res = 0;
+uint16_t sen_data = 0; // Raw input data from sensor
+uint8_t Response = 0;
+int i=0;
+int int_T = 0;
+int dec_T = 0;
+float Temp = 0.0;
+char message[32];
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Configure the System Power */
+  SystemPower_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ICACHE_Init();
+  MX_USART1_UART_Init();
+  MX_USB_DRD_FS_HCD_Init();
+  MX_TIM16_Init();
+  /* USER CODE BEGIN 2 */
+
+  HAL_TIM_Base_Start(&htim16);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	  while (1)
+	  {
+		  DS18B20_Start();
+		  DS18B20_Write(0xCC); // Skip ROM - Master uses this command to address all devices on the bus simultaneously
+		  DS18B20_Write(0x44); // command code for starting single temperature conversion
+		  // resulting data is stored in the 2-byte temp register in the scratch-pad memory
+
+		  HAL_Delay(750);
+		  DS18B20_Start();
+		  DS18B20_Write(0xCC);
+		  DS18B20_Write(0xBE); // requests the sensor to send its current temperature data from temp register
+		  sen_data = DS18B20_Read();
+
+		  // Scratch-pad memory is 16-bit, with thermal data transferred in the first 12-bits LSB first, with the 4 bits predefined for sign bits
+		  int_T = sen_data >> 4; // Integer component stored in the remaining 8 bits
+		  dec_T = sen_data & 0x000F; // Decimal component of data stored in the first 4 bits (starting from LSB)
+		  Temp = int_T + (dec_T/12.0); // Decimal data/resolution - 12-bit resolution
+		  sprintf(message, "\x1b[2J \x1b[HTemperature = %.3f\u00B0C", Temp);
+		  HAL_UART_Transmit(&huart1, (uint8_t*)message, strlen(message), 100);
+		  HAL_Delay(3000);
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_0;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLMBOOST = RCC_PLLMBOOST_DIV4;
+  RCC_OscInitStruct.PLL.PLLM = 3;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 1;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLLVCIRANGE_1;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_PCLK3;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Power Configuration
+  * @retval None
+  */
+static void SystemPower_Config(void)
+{
+
+  /*
+   * Switch to SMPS regulator instead of LDO
+   */
+  if (HAL_PWREx_ConfigSupply(PWR_SMPS_SUPPLY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+/* USER CODE BEGIN PWR */
+/* USER CODE END PWR */
+}
+
+/**
+  * @brief ICACHE Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ICACHE_Init(void)
+{
+
+  /* USER CODE BEGIN ICACHE_Init 0 */
+
+  /* USER CODE END ICACHE_Init 0 */
+
+  /* USER CODE BEGIN ICACHE_Init 1 */
+
+  /* USER CODE END ICACHE_Init 1 */
+
+  /** Enable instruction cache in 1-way (direct mapped cache)
+  */
+  if (HAL_ICACHE_ConfigAssociativityMode(ICACHE_1WAY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_ICACHE_Enable() != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ICACHE_Init 2 */
+
+  /* USER CODE END ICACHE_Init 2 */
+
+}
+
+/**
+  * @brief TIM16 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM16_Init(void)
+{
+
+  /* USER CODE BEGIN TIM16_Init 0 */
+
+  /* USER CODE END TIM16_Init 0 */
+
+  /* USER CODE BEGIN TIM16_Init 1 */
+
+  /* USER CODE END TIM16_Init 1 */
+  htim16.Instance = TIM16;
+  htim16.Init.Prescaler = 160-1;
+  htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim16.Init.Period = 65535;
+  htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim16.Init.RepetitionCounter = 0;
+  htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM16_Init 2 */
+
+  /* USER CODE END TIM16_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USB_DRD_FS Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USB_DRD_FS_HCD_Init(void)
+{
+
+  /* USER CODE BEGIN USB_DRD_FS_Init 0 */
+
+  /* USER CODE END USB_DRD_FS_Init 0 */
+
+  /* USER CODE BEGIN USB_DRD_FS_Init 1 */
+
+  /* USER CODE END USB_DRD_FS_Init 1 */
+  hhcd_USB_DRD_FS.Instance = USB_DRD_FS;
+  hhcd_USB_DRD_FS.Init.dev_endpoints = 8;
+  hhcd_USB_DRD_FS.Init.Host_channels = 8;
+  hhcd_USB_DRD_FS.Init.speed = HCD_SPEED_FULL;
+  hhcd_USB_DRD_FS.Init.phy_itface = HCD_PHY_EMBEDDED;
+  hhcd_USB_DRD_FS.Init.Sof_enable = DISABLE;
+  hhcd_USB_DRD_FS.Init.low_power_enable = DISABLE;
+  hhcd_USB_DRD_FS.Init.vbus_sensing_enable = DISABLE;
+  hhcd_USB_DRD_FS.Init.bulk_doublebuffer_enable = DISABLE;
+  hhcd_USB_DRD_FS.Init.iso_singlebuffer_enable = DISABLE;
+  if (HAL_HCD_Init(&hhcd_USB_DRD_FS) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_DRD_FS_Init 2 */
+
+  /* USER CODE END USB_DRD_FS_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DS18B20_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : USER_BUTTON_Pin */
+  GPIO_InitStruct.Pin = USER_BUTTON_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USER_BUTTON_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : DS18B20_Pin */
+  GPIO_InitStruct.Pin = DS18B20_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DS18B20_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_GREEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+void us_delay(int us) {
+	__HAL_TIM_SET_COUNTER(&htim16, 0);
+	while ((__HAL_TIM_GET_COUNTER(&htim16))<us){}
+}
+
+void SET_PIN_In() {
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	  GPIO_InitStruct.Pin = DS18B20_Pin;
+	  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	  GPIO_InitStruct.Pull = GPIO_NOPULL;
+	  HAL_GPIO_Init(DS18B20_GPIO_Port, &GPIO_InitStruct);
+}
+
+void SET_PIN_Out() {
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	  GPIO_InitStruct.Pin = DS18B20_Pin;
+	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	  GPIO_InitStruct.Pull = GPIO_NOPULL;
+	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	  HAL_GPIO_Init(DS18B20_GPIO_Port, &GPIO_InitStruct);
+}
+
+uint8_t DS18B20_Start() {
+	uint8_t Presence = 0;
+	SET_PIN_Out();
+	__HAL_TIM_SET_COUNTER(&htim16, 0);
+
+	// Transmission of reset pulse by MCU
+	do {
+		HAL_GPIO_WritePin(DS18B20_GPIO_Port, DS18B20_Pin, GPIO_PIN_RESET); // Bus pulled low
+	} while ((__HAL_TIM_GET_COUNTER(&htim16))<=480);
+
+	SET_PIN_In();
+	// Releases the bus to go to receive mode
+	us_delay(60);
+	__HAL_TIM_SET_COUNTER(&htim16, 0); // Resets to 0
+	do {
+		Check_Time = __HAL_TIM_GET_COUNTER(&htim16);
+		// When bus is released, the 5kΩ internal pull-up resistor of sensor pulls the 1-Wire bus high.
+	} while (!(HAL_GPIO_ReadPin(DS18B20_GPIO_Port, DS18B20_Pin))); // Waits for bus to go high
+	//  DS18B20 detects this rising edge, it waits (60us) and then transmits a presence pulse from sensor by pulling the 1-Wire bus low
+	if (Check_Time>=60 && Check_Time<=240)
+		Presence = 1;
+	else
+		Presence = -1;
+	us_delay(480-Check_Time);
+	return Presence;
+}
+
+uint8_t DS18B20_Write(uint8_t data) {
+	// MCU
+	SET_PIN_In();
+	us_delay(5);
+	if (!(HAL_GPIO_ReadPin(DS18B20_GPIO_Port, DS18B20_Pin))) // Checks if bus is low
+			return -1; // Error condition
+
+	for (i=0;i<8;i++) {
+		if ((data&(1<<i))!=0) { // Checks if current data bit transmitted is 1
+			SET_PIN_Out();
+			us_delay(2);
+			HAL_GPIO_WritePin(DS18B20_GPIO_Port, DS18B20_Pin, GPIO_PIN_RESET);
+			us_delay(10);
+			SET_PIN_In();
+			us_delay(55);
+		}
+		else { // current data bit transmitted is 0
+			SET_PIN_Out();
+			us_delay(2);
+			HAL_GPIO_WritePin(DS18B20_GPIO_Port, DS18B20_Pin, GPIO_PIN_RESET);
+			us_delay(60);
+		}
+		SET_PIN_In();
+		us_delay(5);
+	}
+	return 1;
+}
+
+
+uint16_t DS18B20_Read() {
+	uint16_t value = 0;
+	for (i=0;i<15;i++) {
+		// DS18B20
+		SET_PIN_Out();
+		HAL_GPIO_ReadPin(DS18B20_GPIO_Port, DS18B20_Pin); // Checks bus is low
+		us_delay(15);
+		SET_PIN_In();
+		us_delay(5);
+
+		if (!(HAL_GPIO_ReadPin(DS18B20_GPIO_Port, DS18B20_Pin))) { // If the bus is low
+			value |= (0<<i); // Read 0
+			while (!(HAL_GPIO_ReadPin(DS18B20_GPIO_Port, DS18B20_Pin))) {
+				// Waits for the bus to go high
+			}
+		}
+		else {
+			value |= (1<<i); // Read 1
+			us_delay(50);
+		}
+	}
+	return value;
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
